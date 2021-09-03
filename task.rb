f099c74f15ded7d8e411138cceb0b8799c8c366a
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-09-03 23:35:31 +0900

    Q15に回答

--- task.rb
+++ task.rb
@@ -149,7 +149,17 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  
+  if data1.keys.include?(:age)
+    puts "OK"
+  else
+    puts "NG"
+  end
+
+  if data2.keys.include?(:age)
+    puts "OK"
+  else
+    puts "NG"
+  end
 end
 
 def q16
