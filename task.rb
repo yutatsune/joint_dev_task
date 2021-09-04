416783093f770531fe6a60b8c95f1f49fdcb4057
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-09-04 15:02:31 +0900

    Q11を修正

--- task.rb
+++ task.rb
@@ -108,8 +108,7 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-  sports.flatten!
-  sports.uniq!
+  sports.flatten!.uniq!
   puts "ユーザーの趣味一覧"
   sports.each.with_index(1) do |sport, i|
     puts "No#{i} #{sport}"
