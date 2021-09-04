37857c8c6b73092c359ec0151fbec84418378798
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-09-04 15:24:59 +0900

    Q14を修正

--- task.rb
+++ task.rb
@@ -137,11 +137,12 @@ def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-  keys = []
-  data.each_key do |d|
-    keys << d
-  end
-  puts keys
+  # keys = []
+  # data.each_key do |d|
+  #   keys << d
+  # end
+  # puts keys
+  puts data.keys
 end
 
 def q15
