8e9a66bb2cd46407ec427a6d52fbe53b754a5ded
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-09-11 00:32:57 +0900

    Q18に回答

--- task.rb
+++ task.rb
@@ -208,7 +208,14 @@ end
 
 class UserQ18
   # 以下に回答を記載
-
+  attr_accessor :name, :age
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
+  def introduce
+    @age > 10 ? "こんにちは,#{@name}と申します。宜しくお願いいたします。" : "はいさいまいど〜,#{@name}です!!"
+  end
 end
 
 def q18
