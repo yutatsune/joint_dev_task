ffa8f3c4fc3acec7672e71615f1ecd2d273bd76e
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-09-11 00:31:50 +0900

    Q17に回答

--- task.rb
+++ task.rb
@@ -181,7 +181,19 @@ end
 
 class UserQ17
   # 以下に回答を記載
-
+  attr_accessor :name, :age, :gender
+  def initialize(name:, age:, gender:)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+  def info
+    puts <<~TEXT
+    名前:#{@name}
+    年齢:#{@age}
+    性別:#{@gender}
+    TEXT
+  end
 end
 
 def q17
