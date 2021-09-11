1e0da58dbf3d9027cc289639951cc0835f9f74e3
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-09-11 22:34:56 +0900

    Q18を修正

--- task.rb
+++ task.rb
@@ -208,13 +208,17 @@ end
 
 class UserQ18
   # 以下に回答を記載
-  attr_accessor :name, :age
+  # attr_accessor :name, :age
   def initialize(name:, age:)
     @name = name
     @age = age
   end
   def introduce
-    @age > 10 ? "こんにちは,#{@name}と申します。宜しくお願いいたします。" : "はいさいまいど〜,#{@name}です!!"
+    if @age > 10 
+      "こんにちは,#{@name}と申します。宜しくお願いいたします。"
+    else
+      "はいさいまいど〜,#{@name}です!!"
+    end
   end
 end
 
